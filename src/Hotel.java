0452d4265ebf5be4d285bb4098826435f6cd73ce
DESKTOP-JQHEUAO\User <devkim4251@gmail.com>
2023-10-26 16:35:08 +0900

    cancelReservationByUser 추가, uuidMap 제거 (안씀)

--- src/Hotel.java
+++ src/Hotel.java
@@ -4,7 +4,7 @@ public class Hotel {
     private List<HotelRoom> rooms;      // 객실 리스트
     private int hotelAsset;             // 호텔 보유 자산(?) 이게 필요한가?
     private Map<UUID, Reservation> reservationMap = new HashMap<>();    // UUID - 예약
-    private Map<User, List<UUID>> uuidMap = new HashMap<>();            // 고객별별 uuids 리스트
+//    private Map<User, List<UUID>> uuidMap = new HashMap<>();            // 고객별별 uuids 리스트
 
     public Hotel() {
         rooms = new ArrayList<>();
@@ -98,10 +98,10 @@ public class Hotel {
 
         UUID id = UUID.randomUUID();
         reservationMap.put(id, new Reservation(room, user, reservationDate));
-        if (uuidMap.get(user) == null) {
-            uuidMap.put(user, new ArrayList<>());
-        }
-        uuidMap.get(user).add(id);
+//        if (uuidMap.get(user) == null) {
+//            uuidMap.put(user, new ArrayList<>());
+//        }
+//        uuidMap.get(user).add(id);
 
         return id;
     }
@@ -116,14 +116,16 @@ public class Hotel {
         if (reservation == null) {
             System.out.println("잘못된 예약 번호입니다. :" + uuid);
         } else {
-            System.out.println("숙박일자 : " + reservation.getReservationDate());
-            System.out.println("예약자명 : " + reservation.getUser().getName());
-            System.out.println("전화번호 : " + reservation.getUser().getPhoneNumber());
-            System.out.println("방　번호 : " + reservation.getHotelRoom().getUnit());
-            System.out.println("방　크기 : " + reservation.getHotelRoom().getRoomSize());
-            System.out.println("가　　격 : " + reservation.getHotelRoom().getPrice() + " 원");
-            System.out.println("예약일자 : " + reservation.getDate());
-            System.out.println("예약번호 : " + uuid);
+            reservationMap.remove(uuid);
+//            for (User user : uuidMap.keySet()) {
+//                if (uuidMap.get(user).stream()
+//                        .filter(i -> i == UUID.fromString(uuid))
+//                        .findFirst()
+//                        .isPresent() == true) {
+//                    uuidMap.get(user).remove(uuid);
+//                    return;
+//                }
+//            }
         }
 
         sc.close();
